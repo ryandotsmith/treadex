cfaedb074134ab88cb2191355d40033b0c94e394
Ryan Smith <this.ryansmith@gmail.com>
2009-04-26 14:48:22 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :messengers, :has_many => :missions
   map.resources :customers
   map.resources :missions
-  map.resource  :session,
-    :controller => 'clearance/sessions',
-    :only => [:new, :create, :destroy]
-
+ 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
